7cbfe161d50585f5ce9b00e59479cc03efd7cf70
Kierstein Chelsea T. Yabut <kc7345497@gmail.com>
2026-05-23 13:33:43 +0800

    chore: update

---
+++ hq-mobapp/lib/core/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary     = Color(0xFF2F74E0);
+  static const primaryDark = Color(0xFF2463C7);
+  static const bgTop       = Color(0xFF3E86F5);
+  static const bgBottom    = Color(0xFF2D6FDE);
+  static const card        = Colors.white;
+  static const textDark    = Color(0xFF1F2937);
+  static const textMuted   = Color(0xFF6B7280);
+  static const border      = Color(0xFFE5E7EB);
+  static const fieldFill   = Color(0xFFF8FAFC);
+  static const success     = Color(0xFF16A34A);
+  static const warning     = Color(0xFFD97706);
+  static const error       = Color(0xFFDC2626);
+}
