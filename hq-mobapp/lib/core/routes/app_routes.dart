95ce3b93e12b5a5379dec44eb844541614c14242
Kierstein Chelsea T. Yabut <kc7345497@gmail.com>
2026-05-23 13:33:45 +0800

    chore: update

--- hq-mobapp/lib/core/routes/app_routes.dart
+++ hq-mobapp/lib/core/routes/app_routes.dart
@@ -1,43 +1,37 @@
 import 'package:flutter/material.dart';
-import '../../screens/splash_screen.dart';
 import '../../screens/login_screen.dart';
 import '../../screens/register_screen.dart';
-import '../../screens/home_screen.dart';
-import '../../screens/clinic_directory_screen.dart';
-import '../../screens/clinic_detail_screen.dart';
-import '../../screens/queue_status_screen.dart';
-import '../../screens/appointments_screen.dart';
-import '../../screens/book_appointment_screen.dart';
-import '../../screens/notifications_screen.dart';
-import '../../screens/profile_screen.dart';
+import '../../screens/dashboard_screen.dart';
+import '../../screens/queue_monitoring_screen.dart';
+import '../../screens/join_queue_screen.dart';
 import '../../screens/chatbot_screen.dart';
+import '../../screens/book_appointment_screen.dart';
+import '../../screens/appointments_screen.dart';
+import '../../screens/profile_screen.dart';
 
 class AppRoutes {
-  static const splash          = '/';
   static const login           = '/login';
   static const register        = '/register';
-  static const home            = '/home';
-  static const clinicDirectory = '/clinics';
-  static const clinicDetail    = '/clinic-detail';
-  static const queueStatus     = '/queue-status';
-  static const appointments    = '/appointments';
+  static const dashboard       = '/dashboard';
+  static const queueMonitoring = '/queue';
+  static const joinQueue       = '/join-queue';
+  static const chatBot         = '/chat';
   static const bookAppointment = '/book-appointment';
-  static const notifications   = '/notifications';
+  static const appointments    = '/appointments';
   static const profile         = '/profile';
-  static const chatbot         = '/chatbot';
 
   static Map<String, WidgetBuilder> get routes => {
-    splash:          (_) => const SplashScreen(),
     login:           (_) => const LoginScreen(),
     register:        (_) => const RegisterScreen(),
-    home:            (_) => const HomeScreen(),
-    clinicDirectory: (_) => const ClinicDirectoryScreen(),
-    clinicDetail:    (_) => const ClinicDetailScreen(),
-    queueStatus:     (_) => const QueueStatusScreen(),
-    appointments:    (_) => const AppointmentsScreen(),
+    dashboard:       (_) => const DashboardScreen(),
+    queueMonitoring: (_) => const QueueMonitoringScreen(),
+    joinQueue:       (_) => const JoinQueueScreen(),
     bookAppointment: (_) => const BookAppointmentScreen(),
-    notifications:   (_) => const NotificationsScreen(),
+    appointments:    (_) => const AppointmentsScreen(),
     profile:         (_) => const ProfileScreen(),
-    chatbot:         (_) => const ChatbotScreen(),
+    chatBot: (context) => ChatbotScreen(
+      onBookAppointment: () => Navigator.pushNamed(context, bookAppointment),
+      onViewQueue:       () => Navigator.pushNamed(context, queueMonitoring),
+    ),
   };
 }
