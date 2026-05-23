a7b0d4f667983e9ee34680b963d47e0325f886ff
Kierstein Chelsea T. Yabut <kc7345497@gmail.com>
2026-05-23 13:33:41 +0800

    chore: update

--- hq-mobapp/lib/main.dart
+++ hq-mobapp/lib/main.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import 'core/routes/app_routes.dart';
-import 'core/theme/app_theme.dart';
 import 'state/app_state.dart';
+import 'app.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppState(),
-      child: const HealthQueueApp(),
+      child: const MyApp(),
     ),
   );
-}
-
-class HealthQueueApp extends StatelessWidget {
-  const HealthQueueApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HealthQueue+',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      initialRoute: AppRoutes.splash,
-      routes: AppRoutes.routes,
-    );
-  }
 }
