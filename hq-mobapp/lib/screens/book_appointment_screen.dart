3dd031d42612f3efa918b0ebc3badf5c0cf1915b
Kierstein Chelsea T. Yabut <kc7345497@gmail.com>
2026-05-23 23:37:44 +0800

    fix: time slots match server available-slots (8:00AM-4:30PM in 30min increments)

--- hq-mobapp/lib/screens/book_appointment_screen.dart
+++ hq-mobapp/lib/screens/book_appointment_screen.dart
@@ -20,7 +20,7 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
   final reasonCtrl = TextEditingController();
   bool loading = false;
 
-  static const _times = ['8:00 AM','9:00 AM','10:00 AM','11:00 AM','1:00 PM','2:00 PM','3:00 PM','4:00 PM'];
+  static const _times = ['8:00 AM','8:30 AM','9:00 AM','9:30 AM','10:00 AM','10:30 AM','11:00 AM','11:30 AM','1:00 PM','1:30 PM','2:00 PM','2:30 PM','3:00 PM','3:30 PM','4:00 PM','4:30 PM'];
 
   @override
   void initState() {
