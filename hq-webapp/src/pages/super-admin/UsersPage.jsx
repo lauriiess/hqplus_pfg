e515ebab6375b2c8b7c0387b240a9b45bcb2bb21
Kierstein Chelsea T. Yabut <kc7345497@gmail.com>
2026-05-27 23:34:13 +0800

    refactor(css): use super-admin.module.css in UsersPage.jsx

--- hq-webapp/src/pages/super-admin/UsersPage.jsx
+++ hq-webapp/src/pages/super-admin/UsersPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
-import styles from './UsersPage.module.css'
+import styles from './super-admin.module.css'
 
 const ROLE_LABELS = {
   super_admin:    'System Administrator',
